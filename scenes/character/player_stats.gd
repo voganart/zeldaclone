001ec32a35b2a3148ac87e79f69e8ca83c8f536e
Vladimir Grigorov <punktc77@gmail.com>
2025-08-17 18:33:40 -0300

    fix neck bandana, movement parameters looking good

--- scenes/character/player_stats.gd
+++ scenes/character/player_stats.gd
@@ -1,18 +1,26 @@
 extends Resource
 class_name PlayerStats
+@onready var _mesh: Node3D = $character
+
 #jump
 @export var jump_height : float = 2.25
 @export var jump_time_to_peak : float = 0.4
 @export var jump_time_to_descent : float = 0.3
+@export var air_control: float = 0.3
 
-var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
-var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
-var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
-#movement
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+var jump_phase := ""  # '' / 'start' / 'mid'
+
+@onready var anim_player: AnimationPlayer = $character/AnimationPlayer
 @export var base_speed: float = 5.0
 @export var run_speed: float = 8.0
 @export var stop_speed: float = 15.0
 @export var acceleration: float = 0.1
+@export var rot_speed: float = 10.0
 var air_speed: float
 var movement_input: Vector2 = Vector2.ZERO
+var is_running: bool = false
+@export var run_toggle_mode: bool = true
--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody3D
+@onready var _mesh: Node3D = $character
+
 #jump
 @export var jump_height : float = 2.25
 @export var jump_time_to_peak : float = 0.4
 @export var jump_time_to_descent : float = 0.3
 @export var air_control: float = 0.3
+
 
 @onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
 @onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
@@ -25,6 +28,7 @@ func _physics_process(delta: float) -> void:
 	move_logic(delta)
 	jump_logic(delta)
 	rot_char(delta)
+	tilt_character(delta)
 	animation_player()
 	move_and_slide()
 	
@@ -41,14 +45,17 @@ func move_logic(delta):
 	movement_input = Input.get_vector('left',"right", "up", "down")
 	var velocity_2d = Vector2(velocity.x, velocity.z)
 	var is_airborne = not is_on_floor()
-	var control = 1.0 if is_on_floor() else air_control
-	
+	var control = 1.0 if not is_airborne else clamp(air_control, 0.0, 1.0)  # 0 = нет управления, 1 = полное
+
 	if movement_input == Vector2.ZERO:
 		is_running = false
 		
-	var current_speed = air_speed if is_airborne else (run_speed if is_running else base_speed)
-	
+	var current_speed = run_speed if is_running else base_speed
+	if is_airborne:
+		current_speed = air_speed  # сохраняем текущую скорость на воздухе
+
 	if movement_input != Vector2.ZERO:
+		# Лerp с учетом контроля в воздухе
 		velocity_2d = velocity_2d.lerp(movement_input * current_speed, acceleration * control)
 	else:
 		velocity_2d = velocity_2d.move_toward(Vector2.ZERO, stop_speed * delta)
@@ -67,10 +74,16 @@ func rot_char(delta):
 	var vel_2d = Vector2(velocity.x, -velocity.z)
 	if vel_2d.length_squared() > 0.001:
 		var target_angle = vel_2d.angle() + PI / 2
-		var is_sprinting = is_running and is_on_floor()
 		rotation.y = lerp_angle(rotation.y, target_angle, rot_speed * delta)
 
-var is_stopping := false   # флаг состояния торможения
+func tilt_character(delta):
+	var tilt_angle = 15 if is_running else 5
+	var move_vec = Vector3(velocity.x, 0, velocity.z)
+	var local_move = global_transform.basis.inverse() * move_vec
+	var target_tilt = clamp(-local_move.x, -1, 1) * deg_to_rad(tilt_angle)
+	_mesh.rotation.z = lerp_angle(rotation.z, target_tilt, 15 * delta)
+
+var is_stopping := false
 
 func animation_player():
 	var speed_2d := Vector2(velocity.x, velocity.z).length()
@@ -88,22 +101,22 @@ func animation_player():
 		return
 	else:
 		if jump_phase in ["start", "mid"]:
-			anim_player.play("Boy_jump_end", 0.1, lerp(0.5, 1.25, 0.1))
+			anim_player.play("Boy_jump_end", 0, lerp(0.5, 1.25, 0.1))
 			jump_phase = ""
 
 	# Движение по земле
 	if has_input:
 		is_stopping = false
-		if speed_2d > base_speed + 1.5:
-			anim_player.play("Boy_run", 0.8, lerp(0.5, 1.25, speed_2d/6))
-		elif speed_2d > 0.7:
-			anim_player.play("Boy_walk", 0.5, lerp(0.5, 1.25, speed_2d/4))
+		if speed_2d > lerp(base_speed, run_speed, 0.5):
+			anim_player.play("Boy_run", 0.1, lerp(0.5, 1.25, speed_2d/run_speed))
+		elif speed_2d > 0.2:
+			anim_player.play("Boy_walk", 0.3, lerp(0.5, 1.25, speed_2d/base_speed))
 	else:
 		# торможение
-		if speed_2d > 4.0:
+		if speed_2d > 3.6:
 			if not is_stopping:
-				anim_player.play("Boy_stopping", 0.2, lerp(0.5, 1.25, 0.1))
+				anim_player.play("Boy_stopping", 0.2, 0.1)
 				is_stopping = true
 		else:
 			is_stopping = false
-			anim_player.play("Boy_idle", 0.2, 1.0)
+			anim_player.play("Boy_idle", 0.5)
